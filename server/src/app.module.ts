0221356c6052a69a318b4d1903294d450c9e4227
Valentina <aikinara@mail.ru>
2025-10-11 00:13:02 +0300

    Update README with current status

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { SupportRequestModule } from './support-request/support-request.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -9,7 +8,6 @@ import { HotelsModule } from './hotels/hotels.module';
 @Module({
   imports: [
     EventEmitterModule.forRoot(),
-    SupportRequestModule,
     MongooseModule.forRoot('mongodb://localhost:27017/hotel-db'),
     UsersModule,
     AuthModule,
